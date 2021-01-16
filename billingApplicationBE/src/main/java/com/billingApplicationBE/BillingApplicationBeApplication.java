9e5ec3a3cd245890d29fdc06f29699d35025768b
sridh <sridhar.shanu24@gmail.com>
2021-01-16 20:03:49 +0530

    Initial Backend commit

---
+++ billingApplicationBE/src/main/java/com/billingApplicationBE/BillingApplicationBeApplication.java
@@ -0,0 +1,13 @@
+package com.billingApplicationBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingApplicationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingApplicationBeApplication.class, args);
+	}
+
+}
